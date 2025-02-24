d91262da6371a14e375e1312547ef8c4b0f541c9
Andu2023 <andy2023user@gmail.com>
2025-02-24 12:31:47 -0800

    model created

--- test/Application.UnitTest/obj/Debug/net8.0/Application.UnitTest.AssemblyInfo.cs
+++ test/Application.UnitTest/obj/Debug/net8.0/Application.UnitTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f788ba064972d30201cb27e4951cba7fe7419478")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application.UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
